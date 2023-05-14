77f50b64bbe49ab4d6e250f011f29c4f486a6ac8
DeveloperKGH <developerkgh@gmail.com>
2023-05-14 22:30:26 +0900

    :sparkles: feat : 입력된 금액이 1000원 단위가 아닌 경우 IllegalArgumentException 예외가 발생하는지 확인 (ex: 1150)

--- src/main/java/lotto/LottoTicketBox.java
+++ src/main/java/lotto/LottoTicketBox.java
@@ -6,5 +6,8 @@ public class LottoTicketBox {
             throw new IllegalArgumentException("입력된 금액이 1000원 미만일 수 없습니다.");
         }
 
+        if (money % 1000 != 0) {
+            throw new IllegalArgumentException("입력된 금액이 1000원 단위가 아닙니다.");
+        }
     }
 }
--- src/test/java/lotto/LottoTicketBoxTest.java
+++ src/test/java/lotto/LottoTicketBoxTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class LottoTicketBoxTest {
-
     @DisplayName("입력된 금액이 1000원 미만인 경우 IllegalArgumentException 예외가 발생하는지 확인")
     @Test
     void buy_lotto_tickets_with_money_less_than_1000() {
@@ -15,4 +14,11 @@ public class LottoTicketBoxTest {
                 .hasMessageContaining("입력된 금액이 1000원 미만일 수 없습니다.");
     }
 
+    @DisplayName("입력된 금액이 1000원 단위가 아닌 경우 IllegalArgumentException 예외가 발생하는지 확인")
+    @Test
+    void buy_lotto_tickets_with_money_1000원_단위가_아님() {
+        assertThatThrownBy(() -> LottoTicketBox.buyTickets(1150))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력된 금액이 1000원 단위가 아닙니다.");
+    }
 }
