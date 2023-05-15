365963034d995862485186632cbe0bced8424eb9
DeveloperKGH <developerkgh@gmail.com>
2023-05-15 09:08:14 +0900

    :sparkles: feat : 로또 번호 객체가 정상적으로 생성되는지 확인

--- src/main/java/lotto/LottoNumber.java
+++ src/main/java/lotto/LottoNumber.java
@@ -1,13 +1,30 @@
 package lotto;
 
+import java.util.Objects;
+
 public class LottoNumber {
+    public static final int MIN = 1;
+    public static final int MAX = 45;
     private final int number;
 
     public LottoNumber(int number) {
-        if (number < 1 || number > 45) {
+        if (number < MIN || number > MAX) {
             throw new IllegalArgumentException("로또 번호는 1미만 또는 45 초과일 수 없습니다.");
         }
 
         this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,12 +1,20 @@
 package lotto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class LottoNumberTest {
+    @DisplayName("로또 번호 객체가 정상적으로 생성되는지 확인")
+    @Test
+    void create_lotto_number() {
+        assertThat(new LottoNumber(1)).isEqualTo(new LottoNumber(1));
+    }
+
     @DisplayName("로또 번호의 범위가 유효하지 않다면 IllegalArgumentException 예외 발생")
     @ParameterizedTest(name = "testValue : {0}")
     @ValueSource(ints = {0, 46})
