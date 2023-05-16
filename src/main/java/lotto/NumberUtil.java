a20dd26adac3a40b4afc6ba3c495b5e3bee5e59c
DeveloperKGH <developerkgh@gmail.com>
2023-05-17 00:24:01 +0900

    :sparkles: feat : NumberUtil Test Code 추가 - 입력된 문자열이 배열 원소중 숫자가 아닌 다른 형태의 값이 있는경우 IllegalArgumentException 반환되는지 확인 - 입력된 문자열이 배열 원소중 음수가 있는 경우 IllegalArgumentException 반환되는지 확인

--- src/main/java/lotto/NumberUtil.java
+++ src/main/java/lotto/NumberUtil.java
@@ -1,6 +1,10 @@
 package lotto;
 
+import java.util.regex.Pattern;
+
 public class NumberUtil {
+    private static final Pattern NUMERIC_REGEX_PATTERN = Pattern.compile("-?[0-9]+");
+
     public static int[] toInts(String[] values) {
         int[] numbers = new int[values.length];
 
@@ -13,6 +17,10 @@ public class NumberUtil {
     }
 
     private static int toInt(String value) {
-        return Integer.parseInt(value);
+        if (!NUMERIC_REGEX_PATTERN.matcher(value).matches()) throw new IllegalArgumentException("숫자 이외의 값은 입력할 수 없습니다.");
+
+        int number = Integer.parseInt(value);
+        if (number < 0) throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+        return number;
     }
 }
--- src/test/java/lotto/NumberUtilTest.java
+++ src/test/java/lotto/NumberUtilTest.java
@@ -4,11 +4,28 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class NumberUtilTest {
     @DisplayName("입력된 문자열이 배열이 int 배열로 반환되는지 확인")
     @Test
     void toInts_with_valid_value() {
         assertThat(NumberUtil.toInts(new String[]{"1", "2", "3"})).containsExactly(1, 2, 3);
+    }
+
+    @DisplayName("입력된 문자열이 배열 원소중 숫자가 아닌 다른 형태의 값이 있는경우 IllegalArgumentException 반환되는지 확인")
+    @Test
+    void toInts_with_not_numeric_element() {
+        assertThatThrownBy(() -> NumberUtil.toInts(new String[]{"1", "2", "a"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자 이외의 값은 입력할 수 없습니다.");
+    }
+
+    @DisplayName("입력된 문자열이 배열 원소중 음수가 있는 경우 IllegalArgumentException 반환되는지 확인")
+    @Test
+    void toInts_with_negative_number_element() {
+        assertThatThrownBy(() -> NumberUtil.toInts(new String[]{"1", "2", "-3"}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("음수는 입력할 수 없습니다.");
     }
 }
