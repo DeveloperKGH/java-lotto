3902c8eb1845c6d6190248ba275a343848ff2381
DeveloperKGH <developerkgh@gmail.com>
2023-05-17 02:57:12 +0900

    :sparkles: feat : 수익률이 1을 기준으로 이득 or 손해 가 반환되는지 확인

---
+++ src/main/java/lotto/ReturnRateStatus.java
@@ -0,0 +1,34 @@
+package lotto;
+
+import java.util.Arrays;
+
+public enum ReturnRateStatus {
+
+    LOSS("손해", 0, 0.99),
+    PROFIT("이득", 1.01, 2);
+
+    private final String value;
+    private final double startRate;
+    private final double endRate;
+
+    ReturnRateStatus(String name, double startRate, double endRate) {
+        this.value = name;
+        this.startRate = startRate;
+        this.endRate = endRate;
+    }
+
+    public static ReturnRateStatus findByRate(double rate) {
+        return Arrays.stream(ReturnRateStatus.values())
+                .filter(returnRateStatus -> returnRateStatus.betweenRate(rate))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("수익률에 해당하는 범위의 code 가 없습니다."));
+    }
+
+    public boolean betweenRate(double rate) {
+        return this.startRate <= rate && this.endRate >= rate;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/lotto/ReturnRateStatusTest.java
@@ -0,0 +1,16 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReturnRateStatusTest {
+    @DisplayName("수익률이 1을 기준으로 이득 or 손해 가 반환되는지 확인")
+    @ParameterizedTest(name = "수익률 : {0}, 상태 : {1}")
+    @CsvSource(value = {"0.99:손해", "1.01:이득"}, delimiter = ':')
+    void match_numbers_count_money(double rate, String expected) {
+        assertThat(ReturnRateStatus.findByRate(rate).getValue()).isEqualTo(expected);
+    }
+}
